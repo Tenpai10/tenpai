9a26f524b450cf0c7be06260d486140977062d3a
Tenpai10 <pransarbey@gmail.com>
2022-09-30 13:39:18 +0800

    🪲 fix: apos fix

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -9,13 +9,13 @@ export function About() {
       </div>
       <div className="m-4">
         <p className="font-sourceSans text-sm md:text-base text-justify">
-          I love using the extent of my creativity when creating breakthrough
+          {`I love using the extent of my creativity when creating breakthrough
           innovations and software programs. I excel most at developing
           Progressive Web Applications, specifically on front end duties such as
           UI/UX designs and development. I graduated at Technological University
           of the Philippines - Manila in 2022, with a Bachelor's Degree in
           Information Technology, and here I am today, currently working as
-          Digital and Software Solutions Architect since July, 2022.
+          Digital and Software Solutions Architect since July, 2022.`}
         </p>
       </div>
     </section>
